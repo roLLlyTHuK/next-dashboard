3c508fde917c10aead1ff54af67c82d2f19259ca
roLLlyTHuK <ircvipua@gmail.com>
2024-04-24 11:33:34 +0300

    add serv-comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
 
+
+
 export default function Home() {
+  console.log('headers()', headers())
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Home page</h1>
+      <h1 className="text-3xl font-bold underline">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
