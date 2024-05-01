cc76be925c7c8c2d7d34ac632a059943321db81c
roLLlyTHuK <ircvipua@gmail.com>
2024-05-01 14:59:17 +0300

    update

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -13,18 +13,23 @@ export interface StatusLabelProps {
   status: Status;
   disabled?: boolean;
 }
-console.log('CRM_SECRET_KEY', process.env.CRM_SECRET_KEY)
 
-export default function StatusLabel({ children, status, disabled }: StatusLabelProps) {
+export default function StatusLabel({
+  children,
+  status,
+  disabled,
+}: StatusLabelProps) {
   return (
     <div
-      className={clsx('inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
         status === Status.Active && 'text-green-700 bg-green-100',
         status === Status.NotActive && 'text-red-700 bg-red-100',
         status === Status.Pending && 'text-orange-700 bg-orange-100',
         status === Status.Suspended && 'text-blue-700 bg-blue-100',
-        { ['opaccity-75 cursor-not-allowed']: disabled },
-
+        {
+          ['opacity-75 cursor-not-allowed']: disabled,
+        },
       )}
     >
       <div className="w-1 h-1 mr-2 rounded-full bg-current" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-import { headers } from 'next/headers';
-import StatusLabel, { Status } from './components/status-label';
-
-
+import AddCompanyButton from '@/app/components/add-company-button';
 
 export default function Home() {
-  console.log('headers()', headers())
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Home page {new Date().toTimeString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
